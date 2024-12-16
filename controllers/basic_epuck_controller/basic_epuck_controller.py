6ee8fd54bf638336c91ff0024170a07d2442c34b
JayO <bluejayvrstudio@gmail.com>
2024-12-15 23:35:09 -0600

    update

--- controllers/basic_epuck_controller/basic_epuck_controller.py
+++ controllers/basic_epuck_controller/basic_epuck_controller.py
@@ -1,32 +1,38 @@
 """basic_epuck_controller controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+import numpy as np
 
-# create the Robot instance.
 robot = Robot()
 
-# get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+motor_left = robot.getDevice('left wheel motor')
+motor_right = robot.getDevice('right wheel motor')
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
+motor_left.setPosition(float('Inf'))
+motor_right.setPosition(float('Inf'))
+
+# ds = robot.getDevice('ps0')
+# ds.enable(timestep)
+
+ds = []
+for i in range(8):
+    ds.append(robot.getDevice(f'ps{i}'))
+    ds[-1].enable(timestep)
+
+print("Sensors initialized :)")
+
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
 
-    # Process sensor data here.
+    values = []
+    for dist in ds:
+        values.append(dist.getValue())
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+    print(values)
 
-# Enter here exit cleanup code.
+    motor_left.setVelocity(np.pi)
+    motor_right.setVelocity(np.pi)
+
+
+
+
